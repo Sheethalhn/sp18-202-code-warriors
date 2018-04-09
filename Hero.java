dce5d3ebca831fecbbf8b6d371aa410616e74979
rakheegit <34106549+rakheegit@users.noreply.github.com>
2018-04-09 05:31:50 -0700

    Created world and Actor Hero with movements

---
+++ Hero.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown("up"))
+     
+        setLocation(getX(), getY() - 3);
+        //move(3);
+        if (Greenfoot.isKeyDown("down"))
+        setLocation(getX(), getY() + 3);
+        //move(3);
+        if (Greenfoot.isKeyDown("left"))
+        move(-3);
+        if (Greenfoot.isKeyDown("right"))
+        move(3);
+    }    
+}
