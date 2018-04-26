9603600852dba5cf7d4a3b9196eb02e8a2b5bb35
Manisha Shivshette <Manisha.Shivshette@sjsu.edu>
2018-04-26 16:11:36 -0700

    LevelManager added to manage levels

---
+++ Sourcecode/LevelManager.java
@@ -0,0 +1,31 @@
+/**
+ * Write a description of class LevelManager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelManager  
+{
+    // instance variables - replace the example below with your own
+    private int level;
+    
+    
+    /**
+     * Constructor for objects of class LevelManager
+     */
+    public LevelManager()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return 0;
+    }
+}
